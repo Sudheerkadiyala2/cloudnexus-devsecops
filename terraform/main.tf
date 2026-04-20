f20e313ca79168f3f9b1a8358a4dfe0babf23324
kadiyala sudheer kumar <160445120+Sudheerkadiyala2@users.noreply.github.com>
2026-04-20 10:26:13 +0530

    Modify security group for open SSH access
    Updated security group to allow SSH access from any IP address and retained existing configurations.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "sg" {
        from_port=22
        to_port=22
        protocol="tcp"
-       cidr_blocks=["49.206.9.2380/32"]
+       cidr_blocks=["0.0.0.0/0"]
     }
     ingress{
         from_port=5000
